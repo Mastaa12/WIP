5302becf69239722ae65774c1352dfb3df995bba
testacc <test@test.com>
2015-09-04 11:15:02 +0200

    Version3

---
+++ SecondTryJS/SecondTryJS/KlickLibrary.js
@@ -0,0 +1,14 @@
+﻿function MouseOverHandler() {
+color = document.getElementById('tag3').style.backgroundColor ='blue';
+
+}
+
+function KlickMichHandler() {
+    console.log("Klickmichhandler integriert");
+    document.getElementById('tag1').innerHTML = 'Hat geklappt'
+}
+
+function MusicHandler() {
+    document.getElementById('audio').play();
+
+}
